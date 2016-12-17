c1806accd92131fd67fcbf1e42b4c61a39b34f3b
Anurad80 <anoo_radha@outlook.com>
2016-12-17 14:57:03 -0500

    feat: Add Functional test to test that Async task successfully retrieves a non-empty string

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/TestAsyncTask.java
@@ -0,0 +1,26 @@
+package com.udacity.gradle.builditbigger;
+
+
+import android.test.AndroidTestCase;
+import android.util.Log;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class TestAsyncTask extends AndroidTestCase{
+
+    public void testDoInBackground() throws Exception{
+        String joke = "";
+        try {
+            MainActivity mainActivity = new MainActivity();
+            EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask();
+            endpointsAsyncTask.execute(mainActivity);
+            joke = endpointsAsyncTask.get(30, TimeUnit.SECONDS);
+
+            assertNotNull("No joke fetched from server!", joke);
+            assertTrue("No joke fetched from server!", joke.length() > 0);
+        } catch (Exception e){
+            Log.e("TestAsyncTask", "testDoInBackground: Timed out");
+        }
+    }
+}
